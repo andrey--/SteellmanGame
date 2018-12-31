a14b9ae695f634bccfc104085b3d755a13649be2
Andrey Kovalenko <a.y.kovalenko@lohika.com>
2018-12-31 13:49:59 +0200

    Add animation

--- SteellmanGame/MainPage.xaml.cs
+++ SteellmanGame/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Media.Animation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +23,42 @@ namespace SteellmanGame
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        Random random = new Random();
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            AddEnemy();
+        }
+
+        private void AddEnemy()
+        {
+            ContentControl enemy = new ContentControl
+            {
+                Template = Resources["EnemyTemplate"] as ControlTemplate
+            };
+            AnimateEnemy(enemy, 0, playArena.ActualWidth - 100, "(Canvas.Left)");
+            AnimateEnemy(enemy, random.Next((int)playArena.ActualHeight-100), random.Next((int)playArena.ActualHeight - 100), "(Canvas.Top)");
+            playArena.Children.Add(enemy);
+        }
+
+        private void AnimateEnemy(ContentControl enemy, double from, double to, string propertyToAnimate)
+        {
+            Storyboard storyboard = new Storyboard() { AutoReverse = true, RepeatBehavior = RepeatBehavior.Forever };
+            DoubleAnimation animation = new DoubleAnimation()
+            {
+                From = from,
+                To = to,
+                Duration = new Duration(TimeSpan.FromSeconds(random.Next(4, 6)))
+
+            };
+            Storyboard.SetTarget(animation, enemy);
+            Storyboard.SetTargetProperty(animation, propertyToAnimate);
+            storyboard.Children.Add(animation);
+            storyboard.Begin();
         }
     }
 }
