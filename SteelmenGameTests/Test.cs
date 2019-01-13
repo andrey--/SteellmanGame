3b5f4ebaf20119c70bea7d66e350a4cf43e08e01
Andrey Kovalenko <a.y.kovalenko@lohika.com>
2019-01-13 18:06:49 +0200

    Add nunit first test

---
+++ SteelmenGameTests/Test.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteelmenGameTests
+{
+    [TestFixture]
+    public class Test
+    {
+        protected const string AppDriverUrl = "http://127.0.0.1:4723";
+        protected static RemoteWebDriver AppSession;
+
+        [SetUp]
+        public void Setup()
+        {
+            Process.Start("C:\\Program Files (x86)\\Windows Application Driver\\WinAppDriver.exe");
+            DesiredCapabilities cap = new DesiredCapabilities();
+            cap.SetCapability("app", "8da5193e-87c7-4851-983f-6cca351ee226_3k6rj4k30b3e6!App");
+            AppSession = new RemoteWebDriver(new Uri(AppDriverUrl), cap);
+            Assert.IsNotNull(AppSession);
+        }
+
+        [TearDown]
+        public void TestsCleanup()
+        {
+            AppSession.Dispose();
+            AppSession = null;
+        }
+
+        [Test]
+        public void FirstTest()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+    }
+}
